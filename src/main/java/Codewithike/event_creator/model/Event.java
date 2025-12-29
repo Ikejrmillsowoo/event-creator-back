ba27a0ed95360650b31c6beb4c2efc61b003b92f
Isaac Mills-Owoo <ikejrmillsowoo@gmail.com>
2025-12-28 23:31:42 -0500

    configure application properties and add Event and User models

--- src/main/java/Codewithike/event_creator/model/Event.java
+++ src/main/java/Codewithike/event_creator/model/Event.java
@@ -10,4 +10,88 @@ public class Event {
     private String endTime;
     private String location;
     private String createdAt;
+
+    public Event(Long id, Long hostUserId, String title, String description, String eventDate, String startTime, String endTime, String location, String createdAt) {
+        this.id = id;
+        this.hostUserId = hostUserId;
+        this.title = title;
+        this.description = description;
+        this.eventDate = eventDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getHostUserId() {
+        return hostUserId;
+    }
+
+    public void setHostUserId(Long hostUserId) {
+        this.hostUserId = hostUserId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
